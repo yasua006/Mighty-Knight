f87f709dd393b751b8b8e324a288bbde5b14649c
yasua006 <yasua006@osloskolen.no>
2025-11-14 12:48:04 +0100

    Feil rett • Bevegelse

--- rydder.gd
+++ rydder.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
-@export var speed: int = 200
+@export var speed: int = 500
 
 
-func _process(delta: float) -> void:
-	var velocity: Vector2 = Vector2.ZERO
+func _process(_delta: float) -> void:
+	velocity = Vector2.ZERO
 	
 	# bevegelser
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += 20
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
+		velocity.x -= 20
 	if Input.is_action_pressed("move_down"):
-		velocity.y -= 1
+		velocity.y += 20
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 20
 	
 	# bevege
 	move_and_slide()
