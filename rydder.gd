13030d8c31072e9ddccc387d05263d5f359b3a79
tc9ctqxqdf-coder <tc9ctqxqdf@privaterelay.appleid.com>
2025-11-20 10:18:14 +0100

    ny oppgradering
    etter at spilet har krasjet, har vi gått tilbake noen versoner. her er den som virker nå.

--- rydder.gd
+++ rydder.gd
@@ -1,28 +1,33 @@
 extends CharacterBody2D
 
 @export var speed: int = 500
-var JUMP_VELOCITY: int = 400
+var JUMP_VELOCITY: int = -600   # hoppestyrke (negativ for å hoppe oppover)
+var basis := 5
+var gravity: int = 500
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
-	
+
+	# gravity
+	velocity.y += gravity * delta
+
 	# hopp (space)
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
 	# bevegelser (WASD / ←↑↓→)
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 5
+		velocity.x += basis
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 5
+		velocity.x -= basis
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 5
+		velocity.y += basis
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 5
-	
-	
+		velocity.y -= basis
+
 	# håndtere samtidig bevegelser
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
+
 	# bevege med delta
 	move_and_slide()
