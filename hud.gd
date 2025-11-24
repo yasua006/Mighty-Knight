3dc4cfab8f2d02330a45f37fde0e043d2ed5f36f
yasua006 <156091445+yasua006@users.noreply.github.com>
2025-11-24 16:28:40 +0100

    meny funksjon og forbedringer
    innstillinger, start, og exit.
    Bedre håndtering av animasjon slutning

--- hud.gd
+++ hud.gd
@@ -3,18 +3,43 @@ extends Control
 var rydder = null
 var ny_rydder = null
 
+var old_speed: int = 0
+
 
 func _ready() -> void:
 	$level_label.hide()
 	$level_label_timer.start()
+	
+	# få hastigheten før 0 stilling
+	rydder = get_tree().root.get_child(1).get_child(1)
+	old_speed = rydder.speed
+	
+	# 0 hastighet når på menyen
+	rydder.speed = 0
 
 
 func _on_level_label_timer_timeout() -> void:
 	$level_label.show()
 	
-	# vis nivå animasjon
-	$level_label/anim.play("fade_label")
+	# håndtere animasjoner av
+	if not $level_label/anim.active:
+		$level_label.position = Vector2(300, 150)
+		$anim_av_timer.start()
+	else:
+		# vis nivå animasjon
+		$level_label/anim.play("fade_label")
+
+
+func hjelper() -> void:
+	$level_label.hide()
+	
+	# få førrige hastigheten
+	rydder.speed = old_speed
+
+
+func _on_anim_av_timer_timeout() -> void:
+	hjelper()
 
 
 func _on_anim_animation_finished(_anim_name: StringName) -> void:
-	$level_label.hide()
+	hjelper()
--- spill_meny.gd
+++ spill_meny.gd
@@ -1,5 +1,64 @@
 extends Control
 
+var music = null
+var rydder = null
+
+var old_speed: int = 0
+
 
 func _ready() -> void:
-	$VBoxContainer.hide()
+	# ikke spill musikk når på menyen
+	music = get_tree().root.get_child(0)
+	music.playing = false
+	
+	# få hastigheten før 0 stilling
+	rydder = get_tree().root.get_child(1).get_child(1)
+	old_speed = rydder.speed
+	
+	# 0 hastighet når på menyen
+	rydder.speed = 0
+
+
+# ----hoved menyen----
+func _on_start_btn_pressed() -> void:
+	$hoved_container.hide()
+	$bakgrunn_bilde.hide()
+	
+	# start å spille musikk
+	music.playing = true
+	
+	# få førrige hastigheten
+	rydder.speed = old_speed
+
+
+func _on_settings_btn_pressed() -> void:
+	$hoved_container.hide()
+	$settings_container.show()
+
+
+func _on_exit_btn_pressed() -> void:
+	get_tree().quit()
+
+
+# ----innstillinger menyen----
+func _on_animations_btn_toggled(toggled_on: bool) -> void:
+	var anim: AnimationPlayer = get_tree().root.get_child(1).get_child(3).get_child(0).get_child(0)
+	
+	if toggled_on:
+		anim.active = false
+	else:
+		anim.active = true
+
+
+func _on_shadows_btn_toggled(toggled_on: bool) -> void:
+	var lys: PointLight2D = $"/root/main/rydder/PointLight2D"
+	
+	if toggled_on:
+		lys.shadow_enabled = true
+	else:
+		lys.shadow_enabled = false
+
+
+func _on_close_btn_pressed() -> void:
+	$hoved_container.show()
+	$settings_container.hide()
